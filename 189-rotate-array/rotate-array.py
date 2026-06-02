c6ca6ccf736022162a835911dfe36d3dbbd9ae0f
Sai Raj Vadeghar <138302912+sairaj2264@users.noreply.github.com>
2026-06-03 00:05:53 +0530

    Time: 7 ms (56.82%) | Memory: 26.5 MB (70.60%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,38 +4,31 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        k = k % n
-        point = n - k
-
         p1 = 0
-        p2 = point - 1
+
+        k = k%n
+        break_point = n - k
+        p2 = break_point -1
+        
         while(p1<p2):
-            temp = nums[p1]
-            nums[p1] = nums[p2]
-            nums[p2] = temp
+            nums[p1],nums[p2] = nums[p2],nums[p1]
             p1+=1
             p2-=1
         
-        p1 = point
+        p1 = break_point
         p2 = n-1
 
         while(p1<p2):
-            temp = nums[p1]
-            nums[p1] = nums[p2]
-            nums[p2] = temp
+            nums[p1],nums[p2] = nums[p2],nums[p1]
             p1+=1
             p2-=1
-
 
         p1 = 0
         p2 = n-1
         while(p1<p2):
-            temp = nums[p1]
-            nums[p1] = nums[p2]
-            nums[p2] = temp
-            p1 +=1
-            p2 -=1
-
+            nums[p1], nums[p2] = nums[p2], nums[p1]
+            p1+=1
+            p2-=1
+        
         return nums
-
         
