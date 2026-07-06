ed9312e3939e6c3da97a19a351ec1b89cfc661d8
Sai Raj Vadeghar <138302912+sairaj2264@users.noreply.github.com>
2026-07-06 16:34:11 +0530

    Time: 27 ms (26.70%) | Memory: 19.4 MB (89.67%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,45 +1,24 @@
-
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-
-        sample = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-        sample2 =[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-        if sample == candidates:
-            answer = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
-            return answer
         
-        if sample2 == candidates:
-            answer = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2]]
-            return answer
-        candidates.sort()
-        arr = candidates
-        n = len(arr)
-
-        answer = []
         temp = []
-
-        def combiSum2(arr, n, temp, counter, answer, summ, target):
-            if counter == n:
-                if summ == target:
-                    if temp not in answer:
-                        answer.append(temp[:])
-                        return
+        answer = []
+        n = len(candidates)
+        candidates.sort()
+        def combiSum2(nums, n, target, counter, tempSum, tempArr, answer):
+            if tempSum == target:
+                answer.append(tempArr[:])
+            elif tempSum > target:
                 return
-                
-            
-            if summ > target or counter > n:
-                return
-            
-            if counter < n:
-                summ += arr[counter]
-                temp.append(arr[counter])
-            combiSum2(arr, n, temp, counter+1, answer, summ, target)
 
-            summ -= arr[counter]
-            temp.pop()
-            combiSum2(arr, n, temp, counter + 1, answer, summ, target)
-            return
-
-        combiSum2(arr, n, temp, 0, answer, 0, target)
-        return answer
+            for i in range(counter,n):
+                if i != counter and nums[i] == nums[i-1]:
+                    continue
+                tempSum += nums[i]
+                tempArr.append(nums[i])
+                combiSum2(nums, n, target, i + 1,tempSum, tempArr, answer)
+                tempSum -= nums[i]
+                tempArr.pop()
         
+        combiSum2(candidates, n, target, 0,0, temp, answer)
+        return answer
