b06578c480b0a76b4fb222504c545a2dacaa226b
Sai Raj Vadeghar <138302912+sairaj2264@users.noreply.github.com>
2026-07-10 17:00:36 +0530

    Time: 15 ms (5.01%) | Memory: 20.4 MB (31.01%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,55 +1,35 @@
 class Solution:
-    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        
-        n = len(asteroids)
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]-0:
         stack = []
-        for i in range(n):
-            element = asteroids[i]
-            
+        answers = []
 
-            if len(stack) == 0 or element >0:
-                stack.append(element)
+        for i in range (len(asteroids) - 1, -1, -1):
+            if len(stack) == 0 and asteroids[i] > 0:
+                answers.append(asteroids[i])
+            elif len(stack) == 0 and asteroids[i] < 0:
+                stack.append(asteroids[i])
 
-
-
-            else:
-                temp1 = stack[-1]
-
+            elif len(stack) > 0 and asteroids[i] < 0:
+                stack.append(asteroids[i])
+            elif len(stack) > 0 and asteroids[i] > 0:
                 flag = False
-                while( len(stack) > 0 ):
-                    
-                    temp1 = stack[-1]
-
-                    if abs(element) > temp1:
-                        if temp1 > 0:
-                            stack.pop()
-                            flag = True
-                        else:
-                            flag = True
-                            break
-                        
-                        
-
-                    elif abs(element) == temp1:
-                        if element < temp1:
-                            stack.pop()
-                            flag = False
-                            break
-                        else:
-                            stack.append(element)
-                            break
-
+                while(len(stack)!= 0):
+                    if abs(stack[-1]) > asteroids[i]:
+                        break
+                    elif abs(stack[-1]) == asteroids[i]:
+                        stack.pop()
+                        flag = True
+                        break
                     else:
-                        flag = False
-                        break  
+                        stack.pop()
+                if len(stack) == 0:
+                    if flag == False:
+                        answers.append(asteroids[i])
+        if len(stack) > 0:
+            stack = stack[::-1]
+            while(len(stack) > 0):
+                temp = stack.pop()
+                answers.append(temp)
+        answers = answers[:: -1]
+        return answers
 
-
-                if flag == True:
-                    stack.append(element)
-                             
-                
-        return stack
-
-
-
-        
