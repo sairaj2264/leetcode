0d266d0f07e4f1270ec8c7f251a2f107bac2b029
Sai Raj Vadeghar <138302912+sairaj2264@users.noreply.github.com>
2026-07-21 21:47:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (28.01%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -7,17 +7,52 @@
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
 
-        answer = []
+        # answer = []
 
-        def traverse(node):
+        # def traverse(node):
 
-            if node is None:
-                return
+        #     if node is None:
+        #         return
 
-            traverse(node.left)
-            traverse(node.right)
-            answer.append(node.val)
+        #     traverse(node.left)
+        #     traverse(node.right)
+        #     answer.append(node.val)
 
-        traverse(root)
-        return answer
+        # traverse(root)
+        # return answer
+
+        if root is None:
+            return([])
+        stack = []
+
+        preOrder = []
+        inOrder = []
+        postOrder = []
+
+
+
+        stack.append([root, 1])
+
+        while (len(stack) > 0):
+
+            temp = stack.pop()
+            if temp[1] == 1:
+                preOrder.append(temp[0].val)
+                temp[1] += 1
+                stack.append(temp)
+                if temp[0].left is not None:
+                    stack.append([temp[0].left,1])
+            elif temp[1] == 2:
+                inOrder.append(temp[0].val)
+                temp[1] += 1
+                stack.append(temp)
+                if temp[0].right is not None:
+                    stack.append([temp[0].right, 1])
+
+            else:
+                postOrder.append(temp[0].val)
+
+        print(preOrder)
+        print(inOrder)
+        return(postOrder)
         
