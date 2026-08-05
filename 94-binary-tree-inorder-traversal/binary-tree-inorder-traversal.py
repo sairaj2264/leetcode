ed898814719c034b5dd1423e0d9c80558fd29977
Sai Raj Vadeghar <sairaj.vadeghar@gmail.com>
2026-08-05 23:59:28 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (98.98%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,17 +6,51 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        
+
+
+        #Morris In Order Traversal
+
+        current = root
+
         answer = []
-        def traverse(node):
+        while current is not None:
+            if current.left is None:
+                answer.append(current.val)
+                current = current.right
+            
+            else:
+                prev = current.left
 
-            if node is None:
-                return
+                while prev.right is not None and prev.right is not current:
+                    prev = prev.right
+                
+                if prev.right is None:
+                    prev.right = current
+                    current = current.left
+                
+                else:
+                    prev.right = None
+                    answer.append(current.val)
+                    current = current.right
 
-            traverse(node.left)
-            answer.append(node.val)
-            traverse(node.right)
-
-        traverse(root)
         return answer
+
+
+
+        # print(answer)
+                
+
+
+        # answer = []
+        # def traverse(node):
+
+        #     if node is None:
+        #         return
+
+        #     traverse(node.left)
+        #     answer.append(node.val)
+        #     traverse(node.right)
+
+        # traverse(root)
+        # return answer
         
