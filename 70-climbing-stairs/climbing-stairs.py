d3be9afff0f00fe3a438373be503153cc3609c11
Sai Raj Vadeghar <sairaj.vadeghar@gmail.com>
2026-08-07 20:46:51 +0530

    Time: 4 ms (0.75%) | Memory: 19.2 MB (52.33%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,18 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        dp = [-1] * (n + 1)
-
-        def climb ( count, dp):
-            if count <= 3:
-                return count
-            
-            if dp[count] != -1:
-                return dp[count]
-            a = climb(count - 1, dp)
-            b = climb(count - 2, dp)
-            dp[count] = (a + b)
-            return dp[count]
         
-        answer = climb(n, dp)
-        return (answer)
+        
+        dp = [-1] * (n + 1)
+        for i in range(0 , n + 1):
+            if i<=3:
+                dp[i] = i
+            else:
+                ans = (dp[i-1] + dp[i-2])
+                dp[i] = ans
+
+        
+        return dp[n]
