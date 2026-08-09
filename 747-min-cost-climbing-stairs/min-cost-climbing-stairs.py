7171f34089d3384e8accab1ad69ded4f8183326c
Sai Raj Vadeghar <sairaj.vadeghar@gmail.com>
2026-08-09 13:48:07 +0530

    Time: 1 ms (72.23%) | Memory: 20.2 MB (17.25%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        dp = [-1] * len(cost)
+        def recurse(cost, index):
+            if index >= len(cost):
+                return 0
+            
+            if dp[index] == -1:
+                one_step = cost[index] + recurse(cost, index +1) 
+                two_step = cost[index] + recurse (cost, index + 2)
+
+                dp[index] = min(one_step, two_step)
+                return dp[index]
+            else:
+                return dp[index]
+
+        return min(recurse(cost, 0),
+        recurse(cost, 1))
+                
